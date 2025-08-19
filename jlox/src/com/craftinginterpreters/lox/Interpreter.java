07dc8d1d8488d8b3f9480f9967c96a75c3d2e204
johannesehls <johannes.ehls@web.de>
2025-08-19 08:13:16 +0200

    add: support for if statements

--- jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -47,6 +47,17 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
 
+    // If statement.
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(stmt.condition)) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+        return null;
+    }
+
     // Print statement.
     @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
--- jlox/src/com/craftinginterpreters/lox/Parser.java
+++ jlox/src/com/craftinginterpreters/lox/Parser.java
@@ -12,9 +12,10 @@ import static com.craftinginterpreters.lox.TokenType.*;
  *
  * varDecl       → "var" IDENTIFIER ( "=" expression )? ";" ;
  *
- * statement     → exprStmt | printStmt | block;
+ * statement     → exprStmt | ifStmt | printStmt | block;
  *
  * exprStmt      → expression ";" ;
+ * ifStmt        → "if" "(" expression ")" statement ( "else" statement )? ;
  * printStmt     → "print" expression ";" ;
  * block         → "{" declaration* "}" ;
  *
@@ -88,9 +89,24 @@ class Parser {
     }
 
     private Stmt statement() {
+        if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after 'if condition'.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt printStatement() {
